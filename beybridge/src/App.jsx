1dc92dd0758798957b3d6a621c2f3fc9c0a2356c
Steven Oueiss <svo00@mail.aub.edu>
2024-12-04 22:59:53 +0200

    Create React project

---
+++ beybridge/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to BeyBridge</h1>
+        <p>Discover services in Beirut with ease.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
